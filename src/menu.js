c4b090de7e986734b5b0e694656308f59dc8265d
melguelor <melguelor@gmail.com>
2023-06-28 02:02:02 -0300

    Inicio organiza codigo em pastas

--- src/menu.js
+++ src/menu.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import './css.css';
 
 const Menu = () => {
   return (
-    <Navbar expand="lg">
-      
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar expand="lg" className="cor btn-success">
+      <Navbar.Toggle className='cor' aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse className='cor' id="basic-navbar-nav">
         <Nav className="mx-auto display-6">
-          <Nav.Link href="/" >Home</Nav.Link>
-          <Nav.Link href="/simulador">Simulador</Nav.Link>
-          <Nav.Link href="/contatos">Contatos</Nav.Link>
+          <Nav.Link href="/" className='cor' >Home</Nav.Link>
+          <Nav.Link href="/simulador" className='cor'>Simulador</Nav.Link>
+
+          <Nav.Link href="/contatos" className='cor' >Contatos</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
